1afad71bc9bea2617b0678675744460eb318d92b
dev-espinaledu <espinalsolarte7@gmail.com>
2025-03-12 22:09:15 -0500

    "cambios"

--- config/db.js
+++ config/db.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("bd", "root", "contraseña", {
-    host: "localhost",
+    host: "postgresql://fast-training_owner:npg_fgy8nA9aCDXR@ep-jolly-sky-a8ucqzga-pooler.eastus2.azure.neon.tech/fast-training?sslmode=require",
     dialect: "postgres",
-    logging: false,
+    dialectOptions: {
+    ssl: {
+        require: true,
+    rejectUnauthorized: false
+    }
+},
+    logging: false
 });
 
 module.exports = sequelize;
