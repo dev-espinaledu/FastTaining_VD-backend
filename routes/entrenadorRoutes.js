7fd22c782fb012731b9b86e0221379c1db39d243
Johansit0 <johanmunoz319@gmail.com>
2025-03-21 14:33:46 -0500

    check

--- routes/entrenadorRoutes.js
+++ routes/entrenadorRoutes.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const entrenadorController = require("../controllers/entrenadorController");
-const { authMiddleware, verificarEntrenador } = require("../middlewares/authMiddleware");
+const {
+  authMiddleware,
+  verificarEntrenador,
+} = require("../middlewares/authMiddleware");
 
 // Obtener todos los entrenadores (protegido, solo usuarios autenticados)
-router.get("/entrenador/ver", authMiddleware, entrenadorController.verEntrenadores);
+router.get(
+  "/entrenador/ver",
+  authMiddleware,
+  entrenadorController.verEntrenadores,
+);
 
 // Crear un nuevo entrenador (protegido, solo entrenadores pueden hacerlo)
-router.post("/entrenador/crear", authMiddleware, verificarEntrenador, entrenadorController.crearEntrenador);
+router.post("/entrenador/crear", entrenadorController.crearEntrenador);
 
 // Obtener un entrenador específico por ID (protegido, solo usuarios autenticados)
-router.get("/entrenador/:id", authMiddleware, entrenadorController.verEntrenador);
+router.get(
+  "/entrenador/:id",
+  authMiddleware,
+  entrenadorController.verEntrenador,
+);
 
 module.exports = router;
