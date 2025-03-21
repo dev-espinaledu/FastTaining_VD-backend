f6de77b99cba0e9bf5b9d41d9084a350791003c0
dev-espinaledu <espinalsolarte7@gmail.com>
2025-03-21 12:19:22 -0500

    verificarEntrenador

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,13 +2,11 @@ const express = require("express");
 const { login } = require("../controllers/authController");
 const { authMiddleware, verificarAdmin } = require("../middlewares/authMiddleware");
 
-console.log("Middlewares importados:", { authMiddleware, verificarAdmin }); // <-- Agrega esto
-
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/ruta-protegida", authMiddleware, verificarAdmin, (req, res) => {
-  res.json({ message: "Acceso permitido solo para administradores" });
+    res.json({ message: "Acceso permitido solo para administradores" });
 });
 
 module.exports = router;
