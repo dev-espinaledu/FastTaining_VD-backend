51de2258238f86720c6a33dc20a090e62ce3d812
mochi538 <mariar53804@gmail.com>
2025-03-25 13:01:45 -0500

    Ver entrenamiento Individual

--- routes/entrenamientoRoutes.js
+++ routes/entrenamientoRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {generarEntrenamientoIndividual, verEntrenamiento} = require('../controllers/sesionentrenamientoController')
+const {generarEntrenamientoIndividual, verEntrenamientoIndividual, verEntrenamiento} = require('../controllers/sesionentrenamientoController')
 
 router.post("/entrenamiento/crear/:id", generarEntrenamientoIndividual);
-router.get("/entrenamiento/ver/:id", verEntrenamiento);
+router.get("/entrenamiento/ver/:id", verEntrenamientoIndividual);
+router.get("/entrenamiento/", verEntrenamiento);
+
 
 module.exports = router;
