96a0536ded60b12e4e4f6e00c6932525c08496d1
Danny Castro <danllelacrespactr018@gmail.com>
2025-03-27 07:24:43 -0500

    elimine migraciones

--- models/persona.js
+++ models/persona.js
@@ -3,26 +3,13 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Persona extends Model {
     static associate(models) {
-      
-      // Relación 1 a 1 con Usuario
       this.hasOne(models.Usuario, {
         foreignKey: "persona_id",
-        as: "usuarios", 
-        onDelete: "CASCADE", 
-        onUpdate: "CASCADE",
-      });
-      this.hasOne(models.Jugador, {
-        foreignKey: "persona_id",
-        as: "jugador",
+        as: "usuarios",
         onDelete: "CASCADE",
         onUpdate: "CASCADE",
       });
-      this.hasOne(models.Entrenador, {
-        foreignKey: "persona_id",
-        as: "entrenador",
-        onDelete: "CASCADE",
-        onUpdate: "CASCADE",
-      });
+
     }
   }
   Persona.init(
--- routes/sesionesRoutes.js
+++ routes/sesionesRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const sesionEntrenamientoController = require("../controllers/sesionEntrenamientoController");
 
 router.post("/crear/:id", sesionEntrenamientoController.generarEntrenamiento);
-router.get("/sesiones", sesionEntrenamientoController.obtenerSesiones);
+// router.get("/sesiones", sesionEntrenamientoController.obtenerSesiones);
 //router.post("/crea", entrenamientoController.);
 
 module.exports = router;
