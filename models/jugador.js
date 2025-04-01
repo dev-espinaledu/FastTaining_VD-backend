f11d6a0a752fa1e001b7d6e43f60a0b58e8a4e25
Johansit0 <johanmunoz319@gmail.com>
2025-04-01 13:42:21 -0500

    integrando no se que otra vez 😣

--- models/jugador.js
+++ models/jugador.js
@@ -33,7 +33,7 @@ module.exports = (sequelize, DataTypes) => {
       porcentaje_grasa_corporal: DataTypes.DOUBLE,
       porcentaje_masa_muscular: DataTypes.DOUBLE,
       tipo_cuerpo: DataTypes.STRING,
-      fuerza: DataTypes.DOUBLE,
+      potencia_muscular_pierna: DataTypes.DOUBLE,
       velocidad_max: DataTypes.DOUBLE,
       resistencia_aerobica: DataTypes.DOUBLE,
       resistencia_anaerobica: DataTypes.DOUBLE,
