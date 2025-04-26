8667c46468e3f0805f885a47b5cf87e6199bf478
Johansit0 <johanmunoz319@gmail.com>
2025-04-26 13:25:01 -0500

    Limpiar completamente la rama main

--- routes/datosEntrenamientoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {TomarDatosEntrenamiento} = require('../controllers/datosSesionController')
-
-router.post("/sesion/crear", TomarDatosEntrenamiento);
-//router.post("/crea", entrenamientoController.);
-
-module.exports = router;
--- routes/entrenamientoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {generarEntrenamiento} = require('../controllers/sesionEntrenamientoController')
-
-router.post("/entrenamiento/crear/:id", generarEntrenamiento);
-//router.post("/crea", entrenamientoController.);
-
-module.exports = router;
